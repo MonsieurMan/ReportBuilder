0b6e47f5b8dd1d52394d848d0182a89c2d752d28
MonsieurMan <ange.g.picard@gmail.com>
2018-08-20 12:23:05 +0200

    Add syntax highlighting, refactorings

--- src/gulp-plugins/parse-markdown.ts
+++ src/gulp-plugins/parse-markdown.ts
@@ -1,8 +1,16 @@
 import * as marked from 'marked';
 import * as through from 'through2';
 
+import * as hljs from 'highlight.js';
+import javascript from 'highlight.js/lib/languages/javascript';
+
+const markedOptions: marked.MarkedOptions = {
+    smartypants: true, 
+    highlight: (code, lang) =>  hljs.highlight(lang, code).value
+};
+
 export const parseMarkdown = () => through.obj((file, _, cb) => {
-    const html = marked.setOptions({ smartypants: true })(file.contents.toString());
+    const html = marked.setOptions(markedOptions)(file.contents.toString());
     file.contents = Buffer.from(html);
     cb(null, file);
 });
